3748e8f36b437e53dcc7062952a52dd85269cedc
audrio <zerotwo8802@gmail.com>
2025-06-01 21:27:14 +0700

    anjaya

--- ecommer-frozen-food-final-paltform/app/Views/example_j.php
+++ ecommer-frozen-food-final-paltform/app/Views/example_j.php
@@ -6,6 +6,13 @@
     <title>Example je</title>
 </head>
 <body>
-    anjaya 
+    <p>ini adalah paragraf</p>
+    <h1>Adalaha</h1>
+    <h2>Adalah judul 2</h2>
+    <h3>Adalah judul 3</h3>
+    <h4>Adalah judul 4</h4>
+    <h5>Adalah judul 5</h5>
+    <h6>Adalah judul 6</h6>
+    <a href="https://www.google.com">ini adalah link</a>
 </body>
 </html>
