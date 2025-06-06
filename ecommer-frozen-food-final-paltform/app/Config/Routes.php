ad66db7cef6bc538507f0ddbdc4e7b328871ca57
Dipanaaps <divanabungapstariritari.com>
2025-06-06 21:47:34 +0700

    routesnya kak

--- ecommer-frozen-food-final-paltform/app/Config/Routes.php
+++ ecommer-frozen-food-final-paltform/app/Config/Routes.php
@@ -5,4 +5,11 @@ use CodeIgniter\Router\RouteCollection;
 /**
  * @var RouteCollection $routes
  */
-$routes->get('/', 'Pages::index');
+//$routes->get('/', 'Pages::index');
+$routes->get('/', 'AuthController::login');
+
+$routes->get('/login', 'AuthController::login');
+$routes->post('/login', 'AuthController::processLogin');
+
+$routes->get('/register', 'AuthController::register');
+$routes->post('/register', 'AuthController::saveRegister');
