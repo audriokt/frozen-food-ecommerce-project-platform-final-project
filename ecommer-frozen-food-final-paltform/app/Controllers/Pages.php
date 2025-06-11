aff3b06d8c89b96e8afc60cd97e37150d0a02736
Zettavrille17 <zettavrille@gmail.com>
2025-06-11 17:12:11 +0700

    u

--- ecommer-frozen-food-final-paltform/app/Controllers/Pages.php
+++ ecommer-frozen-food-final-paltform/app/Controllers/Pages.php
@@ -4,13 +4,5 @@ namespace App\Controllers;
 
 class Pages extends BaseController
 {
-   public function index()
-    {
-    return view('Customer/Landing_Page_Cus');
-    }
-
-    public function index2()
-    {
-        return view('Customer/Orders_Page');
-    }
+ 
 }
