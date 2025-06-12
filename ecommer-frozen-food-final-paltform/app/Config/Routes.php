0ed0cda9a2e5acbe6a5bc8a613c7fc5d47cd0d6d
audrio <zerotwo8802@gmail.com>
2025-06-12 21:22:20 +0700

    update fitur dan tampilan di cart page

--- ecommer-frozen-food-final-paltform/app/Config/Routes.php
+++ ecommer-frozen-food-final-paltform/app/Config/Routes.php
@@ -26,17 +26,16 @@ $routes->get('/cart/delete/(:any)', 'CartController::delete/$1');
 
 $routes->get('/product/(:any)', 'ProductController::show/$1');
 $routes->get('/product/(:any)', 'ProductController::show/$1');
- 
+
 $routes->get('/profile', 'AuthController::profile');
 $routes->post('/profile/update', 'AuthController::updateProfile');
-   
-$routes->get('/Orders','Pages::index2');
 
+$routes->get('/Orders', 'Pages::index2');
 
-$routes->get('/Orders','Pages::index2');
+$routes->post('/cart/update', 'CartController::update');
+
+$routes->get('/Orders', 'Pages::index2');
 $routes->get('/Checkout_Page', 'Pages::index2');
 
-$routes->get('/Cart_Page','Pages::index2');
-$routes->get('/Orders','Pages::index2');
-
-  
+$routes->get('/Cart_Page', 'Pages::index2');
+$routes->get('/Orders', 'Pages::index2');
