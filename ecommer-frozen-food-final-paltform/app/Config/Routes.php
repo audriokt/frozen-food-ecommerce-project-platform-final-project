975c4d074c21c98ac635a18f66722762df9acaff
Dipanaaps <divanabungapstariritari.com>
2025-06-12 18:48:21 +0700

    Routesnya ya kaks

--- ecommer-frozen-food-final-paltform/app/Config/Routes.php
+++ ecommer-frozen-food-final-paltform/app/Config/Routes.php
@@ -22,6 +22,7 @@ $routes->get('/LandingPage/(:any)', 'ProductController::showCategory/$1');
 
 $routes->get('/cart', 'CartController::index');
 $routes->get('/cart/add/(:any)', 'CartController::add/$1');
+$routes->get('/cart/delete/(:any)', 'CartController::delete/$1');
 
 $routes->get('/Checkout_Page', 'Pages::index2');
 
